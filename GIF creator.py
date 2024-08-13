b409e33ed10e6308af4817644ad714f5e9680bb9
Jan <123250861+Janani-007@users.noreply.github.com>
2024-08-13 17:04:32 +0530

    Delete GIF creator.py

--- GIF creator.py
+++
@@ -1,6 +0,0 @@
-import imageio.v3 as iio
-filenames =['gp1.png','gp2.png','gp3.png','gp4.png','gp5.png','gp6.png','gp7.png','gp8.png','gp9.png','gp10.png']
-images =[ ]
-for i in filenames:
-  images.append(iio.imread(i))
-iio.imwrite('haikyuu.gif',images,duration =500,loop=0)
